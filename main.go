f98f8d656dbe1eefbb42fe2cb7c0ddfe30ebe1cb
Jacob Latonis <latonis@wisc.edu>
2023-04-25 22:03:54 -0500

    regex working for ingredients

--- main.go
+++ main.go
@@ -3,27 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"regexp"
+	"strings"
+
 	"golang.org/x/net/html"
 )
 
 func main() {
-	r := regexp.MustCompile("(\\d+[\\.\\/]*\\d* [a-zA-Z.]+) ([A-Za-z\\d-()'* ]+) \\(([\\$0-9.]+)\\)")
-	testData := `
-	12 oz. shrimp (41-60 size) ($4.99)
-	1 fresh lemon ($0.60)
-	4 cloves garlic ($0.32)
-	2 Tbsp butter ($0.24)
-	1.5 cups long grain white rice ($0.93)
-	2 cups chicken broth ($0.26)
-	1/2 cup water ($0.00)
-	1 tsp Tony Chachere's seasoning* ($0.10)
-	2 Tbsp chopped parsley ($0.09)
-	`
+	r := regexp.MustCompile("(\\d+[\\.\\/]*\\d* [a-zA-Z.]*|\\d+)? ([A-Za-z\\d-(),'*. ]+) \\(([\\$0-9.]+)\\)")
+	testing := parse_page("https://www.budgetbytes.com/peanut-tofu-noodle-bowls/")
 	fmt.Print("recipe incoming!\n\n")
-	matches := r.FindAllStringSubmatch(testData, -1)
-	
+	matches := r.FindAllStringSubmatch(testing, -1)
+
 	for _, match := range matches {
 		// original match, qty, ingredient, price
 		if len(match) != 4 {
@@ -32,16 +23,15 @@ func main() {
 		fmt.Printf("[qty: %s, ingredient: %s, price: %s]\n", match[1], match[2], match[3])
 	}
 	fmt.Println()
-	parse_page("https://www.budgetbytes.com/sriracha-egg-salad/")
 }
 
-func parse_page(url string) {
+func parse_page(url string) string {
 	fmt.Printf("Getting url {%s} ... \n", url)
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("[!!] Error: ", err)
 	}
-	
+
 	defer res.Body.Close()
 
 	doc, err := html.Parse(res.Body)
@@ -50,12 +40,13 @@ func parse_page(url string) {
 	}
 
 	// var ingredients []string
-	var ingredient func (*html.Node)
+	str := ""
+	var ingredient func(*html.Node)
 	ingredient = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "li" {
+		if n.Type == html.ElementNode && n.Data == "ul" {
 			for _, attribute := range n.Attr {
-				if (attribute.Val == "wprm-recipe-ingredient") {
-					fmt.Println(attribute)
+				if attribute.Val == "wprm-recipe-ingredients" {
+					str += navigateUlElement(n)
 				}
 			}
 		}
@@ -64,5 +55,35 @@ func parse_page(url string) {
 			ingredient(c)
 		}
 	}
+
 	ingredient(doc)
+	return str
+}
+
+func navigateUlElement(n *html.Node) string {
+	str := ""
+	for n := n.FirstChild; n != nil; n = n.NextSibling {
+		if n.Type == html.ElementNode && n.Data == "li" {
+			for _, attribute := range n.Attr {
+				if attribute.Val == "wprm-recipe-ingredient" {
+					str += navigateLiElement(n)
+					str += "\n"
+				}
+			}
+		}
+	}
+	return str
+}
+
+func navigateLiElement(n *html.Node) string {
+	str := ""
+	for n := n.FirstChild; n != nil; n = n.NextSibling {
+		if n.Type == html.ElementNode && (n.Data == "span" || n.Data == "a") {
+			str += navigateLiElement(n)
+		}
+		if n.Type == html.TextNode {
+			str += string(n.Data)
+		}
+	}
+	return str
 }
